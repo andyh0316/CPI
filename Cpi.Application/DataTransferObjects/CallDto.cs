11781bd3168283d7319547008619acdac86297f5
Andyh0316 <Andyhong0316@yahoo.com>
2017-03-08 22:16:04 -0800

    Update

---
+++ Cpi.Application/DataTransferObjects/CallDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpi.Application.DataTransferObjects
+{
+    public class CallDto
+    {
+
+    }
+}
